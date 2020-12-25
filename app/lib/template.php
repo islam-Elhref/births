6d9bf0549bef704443ede22ff53321f46dc634de
islam-Elhref <76447019+islam-Elhref@users.noreply.github.com>
2020-12-25 17:16:39 +0200

    add condition to run english or arabic view

--- app/lib/template.php
+++ app/lib/template.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace MYMVC\LIB;
 
 
 class Template
 {
-    private $template ;
-    private $data ;
+    private $template;
+    private $data;
 
 
     public function setData($data)
@@ -18,48 +19,64 @@ class Template
         $this->template = $template;
     }
 
-    private function template_header_start(){
+    private function template_header_start()
+    {
         extract($this->data);
         require_once temp_PATH . 'template_header_start.php';
     }
-    private function template_header_end(){
+
+    private function template_header_end()
+    {
         extract($this->data);
         require_once temp_PATH . 'template_header_end.php';
     }
-    private function template_footer(){
+
+    private function template_footer()
+    {
         extract($this->data);
         require_once temp_PATH . 'template_footer.php';
     }
 
-    private function template_block($view){
+    private function template_block($view)
+    {
         extract($this->data);
-        foreach ($this->template['template'] as $key => $path){
-            if ($key == ':view'){
+        foreach ($this->template['template'] as $key => $path) {
+            if ($key == ':view') {
                 require_once $view;
-            }else{
+            } else {
                 require_once $path;
             }
         }
     }
 
-    private function header_resources(){
+    private function header_resources()
+    {
         $paths = '';
-        foreach ($this->template['header_resources'] as $key => $path){
-            $paths .= '<link rel="stylesheet" href="'.$path.'">' ;
+        $mainLang = 'main_' . $_SESSION['lang'];
+        foreach ($this->template['header_resources'] as $key => $path) {
+            if ($key == "main_ar" || $key == 'main_en') {
+                if ($mainLang == $key ){
+                    $paths .= '<link rel="stylesheet" href="' . $path . '">';
+                }
+            } else {
+                $paths .= '<link rel="stylesheet" href="' . $path . '">';
+            }
         }
         echo $paths;
     }
-    private function footer_resources(){
+
+    private function footer_resources()
+    {
         $script = '';
-        foreach ($this->template['footer_resources'] as $key => $path){
+        foreach ($this->template['footer_resources'] as $key => $path) {
             $script .= "<script src='$path'></script>";
         }
         echo $script;
     }
 
 
-
-    public function render($file_view){
+    public function render($file_view)
+    {
         $this->template_header_start();
         $this->header_resources();
         $this->template_header_end();
