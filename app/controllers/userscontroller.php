223e88fd7b060333a3507a20e61e3a1d7b7ee1f3
islam-Elhref <76447019+islam-Elhref@users.noreply.github.com>
2021-01-05 21:29:05 +0200

    add users as variable from users model :: getall()

--- app/controllers/userscontroller.php
+++ app/controllers/userscontroller.php
@@ -4,11 +4,14 @@
 namespace MYMVC\CONTROLLERS;
 
 
+use MYMVC\MODELS\UsersModel;
+
 class usersController extends AbstractController
 {
 
     public function defaultAction(){
         $this->_language->load('users','default');
+        $this->_data['users'] = UsersModel::getAll();
         $this->view();
     }
 
