0c1e34d2f40d291ce5e1852962834e0a9d910e03
islam-Elhref <76447019+islam-Elhref@users.noreply.github.com>
2021-01-05 21:32:35 +0200

    add filter email

--- app/lib/Filter.php
+++ app/lib/Filter.php
@@ -7,17 +7,25 @@ namespace MYMVC\LIB;
 trait filter
 {
 
-    public function filterInt($input){
-        return filter_var($input , FILTER_SANITIZE_NUMBER_INT);
+    public function filterInt($input)
+    {
+        return filter_var($input, FILTER_SANITIZE_NUMBER_INT);
     }
 
-    public function filterFloat($input){
-        return filter_var($input , FILTER_SANITIZE_NUMBER_FLOAT , FILTER_FLAG_ALLOW_FRACTION);
+    public function filterFloat($input)
+    {
+        return filter_var($input, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     }
 
-    public function filterString($input){
-        $input = htmlspecialchars(strip_tags($input) , ENT_QUOTES , 'UTF-8');
-        return filter_var($input , FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    public function filterString($input)
+    {
+        $input = htmlspecialchars(strip_tags($input), ENT_QUOTES, 'UTF-8');
+        return filter_var($input, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+    public function filterEmail($input)
+    {
+        return filter_var($input, FILTER_SANITIZE_EMAIL);
     }
 
 }
