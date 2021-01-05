3a2f91989467861d713aa6045e7f7d4191139054
islam-Elhref <76447019+islam-Elhref@users.noreply.github.com>
2021-01-05 21:21:45 +0200

    change database name in config

--- app/config/config.php
+++ app/config/config.php
@@ -14,7 +14,7 @@ date_default_timezone_set ('Africa/Cairo' );
 defined('DATABASE_HOST_NAME')             ? null : define('DATABASE_HOST_NAME' , 'localhost');
 defined('DATABASE_USER_NAME')             ? null : define('DATABASE_USER_NAME' , 'root');
 defined('DATABASE_PASSWORD')              ? null : define('DATABASE_PASSWORD' , '');
-defined('DATABASE_DB_NAME')               ? null : define('DATABASE_DB_NAME' , 'mvc');
+defined('DATABASE_DB_NAME')               ? null : define('DATABASE_DB_NAME' , 'store');
 defined('DATABASE_PORT_NUMBER')           ? null : define('DATABASE_PORT_NUMBER' , '3306');
 defined('DATABASE_CONN_DRIVER')           ? null : define('DATABASE_CONN_DRIVER' , '1');
 
